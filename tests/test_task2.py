908118d810e3ec0ef8564ea99a7f10bb6f369f59
Anton <antgusev@yandex.ru>
2024-01-14 12:42:23 +0500

    Changes_5

--- tests/test_task2.py
+++ tests/test_task2.py
@@ -1,16 +1,18 @@
 from unittest import TestCase
-from my_yandisk import YaCreate
+from my_yandisk import YaCreate, token
 
 
 class Test_response(TestCase):
     def test_response_ok(self):
-        res = YaCreate(lambda: self.create_folder())
+        # res = YaCreate(lambda: self.create_folder().response)
+        res = YaCreate(token).create_folder(self).response
         expected = '<Response [201]>'
         self.assertEqual(res, expected)
         print(f'Код ответа соответствует ', {res})
         
     def test_response_error(self):
-        res = YaCreate(lambda: self.create_folder())
+        # res = YaCreate(lambda: self.create_folder().response)
+        res = YaCreate(token).create_folder(self).response
         expected = '<Response [409]>'
         self.assertEqual(res, expected)
         print(f'Код ответа соответствует ', {res})
